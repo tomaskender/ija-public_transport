78c1c257274c678f170b592bf9eabddf81c0f5c9
Tomáš Ďuriš <xduris05@vutbr.cz>
2020-03-29 22:45:42 +0200

    IJA stop on stret  WIP

--- src/data/implementations/CONFIG.java
+++ src/data/implementations/CONFIG.java
@@ -1,8 +1,17 @@
 package data.implementations;
 
+import data.interfaces.Stop;
+import data.interfaces.Street;
+
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class CONFIG {
     public static double EXPECTED_STOP_TIME = 2;
     public static LocalTime CURRENT_TIME;
+    public static Map<String, Street> streets = new HashMap<>();
+    public  static List<Stop> stops = new ArrayList<>();
 }
--- src/sample/XMLParser.java
+++ src/sample/XMLParser.java
@@ -1,15 +1,22 @@
 package sample;
 
 import java.io.InputStream;
+import javax.security.auth.login.Configuration;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
 
+import data.implementations.CONFIG;
+import data.implementations.MyStop;
+import data.implementations.MyStreet;
 import data.interfaces.Coordinate;
+import data.interfaces.Stop;
+import data.interfaces.Street;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 import org.w3c.dom.Element;
 
+import java.sql.Array;
 import java.util.*;
 
 public class XMLParser {
@@ -18,6 +25,7 @@ public class XMLParser {
         Map<String, Coordinate> stops = new HashMap<>();
         Map<String, List> streets = new HashMap<>();
         Map<String, List> links = new HashMap<>();
+        Map<String, String> stops_on_streets = new HashMap<>();
         try {
             InputStream input = XMLParser.class.getResourceAsStream("source.xml");
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
@@ -36,6 +44,7 @@ public class XMLParser {
                         Node node1 = stopList.item(j);
                         if(node1.getNodeType() == node1.ELEMENT_NODE){
                             Element stop = (Element) node1;
+                            stops_on_streets.put(stop.getTextContent(), stop.getAttribute("street"));
                             stops.put(stop.getTextContent(), Coordinate.CreateCoordinate(Integer.parseInt(stop.getAttribute("cord_X")), Integer.parseInt(stop.getAttribute("cord_Y"))));
                         }
                     }
@@ -67,18 +76,61 @@ public class XMLParser {
 
                 if(nNode.getNodeType() == Node.ELEMENT_NODE){
                     Element element = (Element) nNode;
-                    List<String> route = new ArrayList<>();
+                    List<String> route;
+                    List<List> attribs = new ArrayList<>();
+                    List<String> link_timesList = new ArrayList<>();
                     route = Arrays.asList(element.getAttribute("route").split("\\s+"));
                     if(route.get(0) != "") {
-                        links.put(element.getTextContent().split("\\s+")[0], route);
+                        attribs.add(route);
                     }
+                    NodeList link_times = element.getElementsByTagName("link_t");
+                    for(int j = 0; j < link_times.getLength(); j++){
+                        Node node1 = link_times.item(j);
+                        if(node1.getNodeType() == node1.ELEMENT_NODE){
+                            Element link_time = (Element) node1;
+                            link_timesList.add(link_time.getAttribute("start"));
+                        }
+                    }
+                    attribs.add(link_timesList);
+                    List<List> links_stops = new ArrayList<>();
+                    NodeList link_stops = element.getElementsByTagName("stop");
+                    for(int k = 0; k < link_stops.getLength(); k++){
+                        Node node1 = link_stops.item(k);
+                        if(node1.getNodeType() == node1.ELEMENT_NODE){
+                            Element link_stop = (Element) node1;
+                            List<String> stop = new ArrayList<>();
+                            stop.add(link_stop.getTextContent());
+                            stop.add(link_stop.getAttribute("time"));
+                            links_stops.add(stop);
+                        }
+                    }
+                    attribs.add(links_stops);
+                    links.put(element.getTextContent().split("\\s+")[0], attribs);
                 }
             }
-            System.out.print(stops);
-            System.out.print(streets);
-            System.out.print(links);
         } catch (Exception e) {
             e.printStackTrace();
         }
+        for(String key : streets.keySet()){
+            Coordinate[] coords = new Coordinate[streets.get(key).size()];
+            CONFIG.streets.put(key,MyStreet.CreateStreet(key, coords));
+        }
+        for(String stop_key : stops.keySet()){
+            Stop stop = MyStop.CreateStop(stop_key, stops.get(stop_key));
+            CONFIG.stops.add(stop);
+            for(String street_key : stops_on_streets.keySet()){
+                if(stop_key.equals(street_key)){
+                   // Street street = CONFIG.streets.get(stops_on_streets.get(street_key));
+                   // stop.SetStreet(street);
+                }
+            }
+        }
+        System.out.print(CONFIG.streets);
+        System.out.print("\n");
+        System.out.print(CONFIG.stops);
+        System.out.print("\n");
+        System.out.print(links);
+        System.out.print("\n");
+        System.out.print(stops_on_streets);
     }
 }
