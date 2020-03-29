4055bb3e5e585756f69d90ff5ca5a8cee33d82d0
Tomáš Ďuriš <xduris05@vutbr.cz>
2020-03-29 15:59:56 +0200

    IJA parser lines WIP

--- src/sample/XMLParser.java
+++ src/sample/XMLParser.java
@@ -3,20 +3,21 @@ package sample;
 import java.io.InputStream;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
+
+import data.interfaces.Coordinate;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 import org.w3c.dom.Element;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class XMLParser {
 
     public static void main(String[] args) {
-        Map<String, List> stops = new HashMap<String, List>();
+        Map<String, Coordinate> stops = new HashMap<>();
+        Map<String, List> streets = new HashMap<>();
+        Map<String, List> links = new HashMap<>();
         try {
             InputStream input = XMLParser.class.getResourceAsStream("source.xml");
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
@@ -35,15 +36,47 @@ public class XMLParser {
                         Node node1 = stopList.item(j);
                         if(node1.getNodeType() == node1.ELEMENT_NODE){
                             Element stop = (Element) node1;
-                            List<String> coordinates = new ArrayList<String>();
-                            coordinates.add(stop.getAttribute("cord_X"));
-                            coordinates.add(stop.getAttribute("cord_Y"));
-                            stops.put(stop.getTextContent(), coordinates);
+                            stops.put(stop.getTextContent(), Coordinate.CreateCoordinate(Integer.parseInt(stop.getAttribute("cord_X")), Integer.parseInt(stop.getAttribute("cord_Y"))));
                         }
                     }
-                    System.out.print(stops);
                 }
             }
+            nList = doc.getElementsByTagName("streets");
+            for(int i = 0; i < nList.getLength(); i++){
+                Node nNode = nList.item(i);
+                if(nNode.getNodeType() == Node.ELEMENT_NODE){
+                    Element element = (Element) nNode;
+                    NodeList streetList = element.getElementsByTagName("street");
+
+                    for(int j = 0; j < streetList.getLength(); j++){
+                        Node node1 = streetList.item(j);
+                        if(node1.getNodeType() == node1.ELEMENT_NODE){
+                            Element street = (Element) node1;
+                            List<String> begin_end = new ArrayList<>();
+                            List<Coordinate> coordinates = new ArrayList<>();
+                            coordinates.add(Coordinate.CreateCoordinate(Integer.parseInt(street.getAttribute("begin").split("\\s+")[0]), Integer.parseInt(street.getAttribute("begin").split("\\s+")[1])));
+                            coordinates.add(Coordinate.CreateCoordinate(Integer.parseInt(street.getAttribute("end").split("\\s+")[0]), Integer.parseInt(street.getAttribute("end").split("\\s+")[1])));
+                            streets.put(street.getTextContent(), coordinates);
+                        }
+                    }
+                }
+            }
+            nList = doc.getElementsByTagName("link");
+            for(int i = 0; i < nList.getLength(); i++){
+                Node nNode = nList.item(i);
+
+                if(nNode.getNodeType() == Node.ELEMENT_NODE){
+                    Element element = (Element) nNode;
+                    List<String> route = new ArrayList<>();
+                    route = Arrays.asList(element.getAttribute("route").split("\\s+"));
+                    if(route.get(0) != "") {
+                        links.put(element.getTextContent().split("\\s+")[0], route);
+                    }
+                }
+            }
+            System.out.print(stops);
+            System.out.print(streets);
+            System.out.print(links);
         } catch (Exception e) {
             e.printStackTrace();
         }
