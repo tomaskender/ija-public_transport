506930662414932cbc6ea07d541e43bb58c898bb
Tomáš Ďuriš <xduris05@vutbr.cz>
2020-04-02 19:30:57 +0200

    GUI streets and stops

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,22 +1,50 @@
 package sample;
 
 import data.implementations.CONFIG;
+import data.interfaces.Stop;
+import data.interfaces.Street;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalTime;
+import java.util.stream.Stream;
 
 public class Controller {
     public Button pauseButton;
     public TextField timeLabel;
+    @FXML
+    AnchorPane field;
+
 
 
     boolean isPaused = true;
+
+    @FXML
+    public void LoadStreets(){
+        for (Street street : CONFIG.streets.values()){
+            field.getChildren().add(new Line(street.getBegin().getX(),street.getBegin().getY(),street.getEnd().getX(),street.getEnd().getY()));
+
+        }
+    }
+
+    @FXML
+    public void LoadStops(){
+        for (Stop stop : CONFIG.stops.values()){
+            Circle circle = new Circle();
+            circle.setCenterX(stop.getCoordinate().getX());
+            circle.setCenterY(stop.getCoordinate().getY());
+            circle.setRadius(5);
+            field.getChildren().add(circle);
+        }
+    }
 
     @FXML
     public void initialize() {
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,26 +3,41 @@ package sample;
 import data.implementations.CONFIG;
 import data.interfaces.Vehicle;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Shape;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.time.temporal.ChronoUnit;
 import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Main extends Application {
+    @FXML
+    AnchorPane field;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
+        Parent root = loader.load();
         primaryStage.setTitle("Public Transport Tracker");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1000, 700));
         primaryStage.show();
 
-
+        Controller controller = loader.getController();
+        controller.LoadStreets();
+        controller.LoadStops();
         // moving vehicles on map
         Timer timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
